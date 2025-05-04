842b14113cbf289b175671ee413dd33e8a542684
mahammedjunedattar <123448134+mahammedjunedattar@users.noreply.github.com>
2025-05-04 04:27:49 -0700

    Delete app/middleware directory

--- app/middleware/logging.js
+++
@@ -1,8 +0,0 @@
-// middleware/logging.js
-export async function middleware(req) {
-    console.log({
-      user: req.user?.email,
-      path: req.nextUrl.pathname,
-      timestamp: new Date().toISOString()
-    });
-  }
